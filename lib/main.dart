02da27338aa5521a142387024cd382aba40e1bf4
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-27 12:35:08 +0530

    windows setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:platform_ui/android_app_view/android_app_view.dart';
 import 'package:platform_ui/ios_view/ios_view.dart';
 import 'package:platform_ui/macos_view/macos_view.dart';
 import 'package:platform_ui/win_view/win_view.dart';
+import 'package:window_manager/window_manager.dart';
 
 /// This method initializes macos_window_utils and styles the window.
 Future<void> _configureMacosWindowUtils() async {
@@ -13,13 +14,32 @@ Future<void> _configureMacosWindowUtils() async {
   await config.apply();
 }
 
+Future<void> _configureWindowSize({Size minSize = const Size(1000, 550)}) async {
+  await WindowManager.instance.ensureInitialized();
+  WindowOptions windowOptions = WindowOptions(
+    size: minSize,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+    minimumSize: minSize,
+    windowButtonVisibility: false,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions).then((_) async {
+    await windowManager.show();
+    await windowManager.focus();
+    await windowManager.setPreventClose(true);
+  });
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   switch (Platform.operatingSystem) {
     case 'macos':
       _configureMacosWindowUtils();
       runApp(const MacOSView());
       break;
     case 'windows':
+      _configureWindowSize();
       runApp(const WinView());
       break;
     case 'ios':
--- lib/win_view/win_view.dart
+++ lib/win_view/win_view.dart
@@ -1,4 +1,6 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:window_manager/window_manager.dart';
 
 class WinView extends StatelessWidget {
   const WinView({super.key});
@@ -6,12 +8,105 @@ class WinView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
-      return const FluentApp(
+      return FluentApp(
         title: 'TempBox',
         debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.system,
-        home: SizedBox.shrink(),
+        darkTheme: FluentThemeData(brightness: Brightness.dark),
+        home: const WinHome(),
       );
     });
+  }
+}
+
+class WinHome extends StatelessWidget {
+  const WinHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationView(
+      appBar: NavigationAppBar(
+        leading: const FlutterLogo(),
+        title: const DragToMoveArea(child: Align(alignment: AlignmentDirectional.centerStart, child: Text('TempBox'))),
+        actions: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Tooltip(
+            message: 'Share message',
+            child: IconButton(icon: const Icon(FluentIcons.share, size: 20), onPressed: () {}),
+          ),
+          const SizedBox(width: 10),
+          Tooltip(
+            message: 'Delete message',
+            child: IconButton(icon: const Icon(CupertinoIcons.trash, size: 20), onPressed: () {}),
+          ),
+          const SizedBox(width: 10),
+          const WindowButtons(),
+        ]),
+      ),
+      pane: NavigationPane(
+        header: Card(
+          margin: const EdgeInsets.only(right: 15, left: 8, bottom: 15),
+          padding: EdgeInsets.zero,
+          child: ListTile(
+            title: Text('New Address', style: FluentTheme.of(context).typography.body),
+            trailing: const Icon(CupertinoIcons.add_circled_solid),
+            onPressed: () {},
+          ),
+        ),
+        onItemPressed: (index) {},
+        size: NavigationPaneSize(openWidth: MediaQuery.of(context).size.width / 5, openMinWidth: 250, openMaxWidth: 250),
+        items: [
+          PaneItem(icon: const Icon(CupertinoIcons.tray), title: const Text('Folder One'), body: const SizedBox.shrink()),
+          PaneItem(icon: const Icon(CupertinoIcons.tray), title: const Text('Folder Two'), body: const SizedBox.shrink()),
+        ],
+        displayMode: PaneDisplayMode.open,
+        toggleable: true,
+        selected: 0,
+      ),
+      paneBodyBuilder: (item, child) {
+        final name = item?.key is ValueKey ? (item!.key as ValueKey).value : null;
+        return FocusTraversalGroup(key: ValueKey('body$name'), child: const SelectedNoteView());
+      },
+    );
+  }
+}
+
+class SelectedNoteView extends StatelessWidget {
+  const SelectedNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 280,
+          child: ScaffoldPage(
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            content: Builder(builder: (context) {
+              return ListView.separated(
+                itemCount: 20,
+                itemBuilder: (ctx, index) => ListTile(title: Text('Note ${index + 1}'), subtitle: const Text('note data')),
+                separatorBuilder: (context, index) => const Divider(),
+              );
+            }),
+          ),
+        ),
+        const Divider(direction: Axis.vertical),
+        const Expanded(child: ScaffoldPage(padding: EdgeInsets.symmetric(vertical: 3), content: Center(child: Text('Note data')))),
+      ],
+    );
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final FluentThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 50,
+      child: WindowCaption(brightness: theme.brightness, backgroundColor: Colors.transparent),
+    );
   }
 }
