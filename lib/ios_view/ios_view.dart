afc8764bcdad0a39205ecae1ac5231e2e3400ba6
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-27 12:51:11 +0530

    ios setup complete

--- lib/ios_view/ios_view.dart
+++ lib/ios_view/ios_view.dart
@@ -14,7 +14,78 @@ class IosView extends StatelessWidget {
         DefaultWidgetsLocalizations.delegate,
         DefaultCupertinoLocalizations.delegate,
       ],
-      home: SizedBox.shrink(),
+      home: IosHome(),
+    );
+  }
+}
+
+class IosHome extends StatelessWidget {
+  const IosHome({super.key});
+
+  _navigateToNotesList(BuildContext context) {
+    Navigator.of(context).push(CupertinoPageRoute(builder: (context) => const NotesList()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemGroupedBackground,
+      child: CustomScrollView(
+        slivers: [
+          const CupertinoSliverNavigationBar(largeTitle: Text('Platform UI')),
+          SliverList.list(children: [
+            CupertinoListSection.insetGrouped(
+              children: [
+                CupertinoListTile(title: const Text('Folder One'), onTap: () => _navigateToNotesList(context)),
+                CupertinoListTile(title: const Text('Folder Two'), onTap: () => _navigateToNotesList(context)),
+              ],
+            )
+          ])
+        ],
+      ),
+    );
+  }
+}
+
+class NotesList extends StatelessWidget {
+  const NotesList({super.key});
+
+  _navigateToNoteDetail(BuildContext context) {
+    Navigator.of(context).push(CupertinoPageRoute(builder: (context) => const NoteDetail()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemGroupedBackground,
+      child: CustomScrollView(
+        slivers: [
+          const CupertinoSliverNavigationBar(largeTitle: Text('Notes List')),
+          SliverList.list(
+            children: [
+              CupertinoListSection.insetGrouped(
+                children: List.generate(30, (index) {
+                  return CupertinoListTile(title: Text('Note ${index + 1}'), onTap: () => _navigateToNoteDetail(context));
+                }),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NoteDetail extends StatelessWidget {
+  const NoteDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.systemGroupedBackground,
+      child: CustomScrollView(
+        slivers: [CupertinoSliverNavigationBar(largeTitle: Text('Note Detail')), SliverToBoxAdapter(child: Center(child: Text('Note data')))],
+      ),
     );
   }
 }
