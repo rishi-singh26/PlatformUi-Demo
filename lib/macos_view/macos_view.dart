2667925905f45f672fcf49596168c4b88bd894e9
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-27 12:13:07 +0530

    Updated macos view

--- lib/macos_view/macos_view.dart
+++ lib/macos_view/macos_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart' show ThemeMode;
+import 'package:flutter/material.dart' show Divider, ThemeMode;
 import 'package:macos_ui/macos_ui.dart';
-import 'package:platform_ui/macos_view/mac_os_home.dart';
 
 class MacOSView extends StatelessWidget {
   const MacOSView({super.key});
@@ -13,6 +12,102 @@ class MacOSView extends StatelessWidget {
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: MacOsHome(),
+    );
+  }
+}
+
+class MacOsHome extends StatelessWidget {
+  const MacOsHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final typography = MacosTypography.of(context);
+    return MacosWindow(
+      sidebar: Sidebar(
+        top: Container(
+          margin: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+          clipBehavior: Clip.hardEdge,
+          decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5))),
+          child: CupertinoListTile(
+            title: Text('New Folder', style: typography.body),
+            trailing: MacosIcon(
+              CupertinoIcons.add_circled_solid,
+              size: 18,
+              color: CupertinoColors.systemGrey.resolveFrom(context),
+            ),
+            backgroundColor: CupertinoColors.systemGrey2.resolveFrom(context).withAlpha(44),
+            backgroundColorActivated: CupertinoColors.systemGrey4.resolveFrom(context),
+            onTap: () {},
+          ),
+        ),
+        minWidth: 240,
+        maxWidth: 270,
+        builder: (context, scrollController) => SidebarItems(
+          currentIndex: 0,
+          onChanged: (i) {},
+          scrollController: scrollController,
+          items: const [
+            SidebarItem(leading: MacosIcon(CupertinoIcons.tray, size: 15), label: Text('Folder One')),
+            SidebarItem(leading: MacosIcon(CupertinoIcons.tray, size: 15), label: Text('Folder Two')),
+          ],
+        ),
+      ),
+      child: const SelectedFolderView(),
+    );
+  }
+}
+
+class SelectedFolderView extends StatefulWidget {
+  const SelectedFolderView({super.key});
+
+  @override
+  State<SelectedFolderView> createState() => _SelectedFolderViewState();
+}
+
+class _SelectedFolderViewState extends State<SelectedFolderView> {
+  double ratingValue = 0;
+  double capacitorValue = 0;
+  double sliderValue = 0.3;
+
+  @override
+  Widget build(BuildContext context) {
+    return MacosScaffold(
+      toolBar: ToolBar(
+        title: Builder(builder: (context) => const Text("Folder One")),
+        actions: [
+          ToolBarIconButton(
+            icon: const MacosIcon(CupertinoIcons.share),
+            onPressed: () {},
+            label: 'Share',
+            showLabel: false,
+            tooltipMessage: 'Share note',
+          ),
+          ToolBarIconButton(
+            icon: const MacosIcon(CupertinoIcons.trash),
+            onPressed: () {},
+            label: 'Delete',
+            showLabel: false,
+            tooltipMessage: 'Delete note',
+          ),
+        ],
+      ),
+      children: [
+        ResizablePane(
+          minSize: 200,
+          startSize: 300,
+          windowBreakpoint: 700,
+          resizableSide: ResizableSide.right,
+          builder: (context, scrollcontroller) {
+            return ListView.separated(
+              controller: scrollcontroller,
+              itemCount: 20,
+              itemBuilder: (ctx, index) => MacosListTile(title: Text('Note ${index + 1}'), subtitle: const Text('note data')),
+              separatorBuilder: (context, index) => const Divider(indent: 15, endIndent: 15, thickness: 0, height: 0),
+            );
+          },
+        ),
+        ContentArea(builder: (context, scrollController) => const Center(child: Text('Note Data'))),
+      ],
     );
   }
 }
